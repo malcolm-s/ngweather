3e644e8e8408fdb878edd5ec6e186525f00af667
Malcolm Stone <malcolm.stone@geeks.ltd.uk>
2016-01-12 12:55:41 +0000

    initial city api logic

--- src/services/city-service.js
+++ src/services/city-service.js
@@ -1,6 +1,18 @@
-angular.module('app').factory('CityService', function() {
+angular.module('app').factory('CityService', function($http) {
   return {
     all: function() {
+      var url = "https://raw.githubusercontent.com/David-Haim/CountriesToCitiesJSON/master/countriesToCities.json";
+
+      $http.get(url).then(function(res) {
+        var data = res.data["United Kingdom"];
+        console.log(data);
+        var cityData = data.map(function(city) {
+          return { name: city, country: 'uk' };
+        });
+
+        console.log(cityData);
+      });
+
       return [
         { name: 'London', country: 'uk' },
         { name: 'Paris', country: 'fr' },
